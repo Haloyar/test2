753b55e17e0954ed879b7bee49e4ef7d6bab7fd7
Haloyar <yarzoo@bk.ru>
2020-12-08 13:31:58 +0400

    All done2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Преобразованиее имени");
         boolean isCorrectName = false;
         while (!isCorrectName) {
             String name = scanner.nextLine(); //Считывает строку из System.in
